dcf99466bd588317ebe56f6fa604e4bbdceedc27
arriolaleo <riblarsv@gmail.com>
2022-03-23 19:23:00 +0600

    commit now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import Recipies from './components/Recipies';
 import {Routes,Route} from 'react-router-dom'
 import Recipe from './components/Recipe'
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Form/>
-      <Recipies/>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/Recipe/:recipe" element={<Recipe />}/>
       </Routes>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Form from "../components/Form"
+import Recipies from "../components/Recipies"
+function Home(){
+    return(
+        <>
+        <Form/>
+        <Recipies/>
+        </>
+    )
+}
+export default Home
